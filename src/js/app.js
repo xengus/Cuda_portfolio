d121b197a3651736f22c466a9193a094b0476ab5
xengus <serjiox13@gmail.com>
2019-08-29 22:30:46 +0300

    some buttons

--- src/js/app.js
+++ src/js/app.js
@@ -4,5 +4,11 @@ function handler(event) {
   a.style.backgroundColor = 'red';
 }
 
-
-
+let button = document.querySelector('.portfolio_buttons')
+button.addEventListener('click', handler1);
+function handler1(event) {
+  setTimeout (function () {
+    let target = event.target;
+    target.style.backgroundColor = 'cyan';
+    }, 5000)
+}

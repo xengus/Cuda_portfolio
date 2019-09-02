a68e3aa071c3f27268e530830ff89d3108ccd1ef
xengus <serjiox13@gmail.com>
2019-09-02 23:31:04 +0300

    Updated buttons

--- src/js/app.js
+++ src/js/app.js
@@ -1,42 +1,19 @@
-const buttonHeader = document.querySelector('button'); //header (не сработает)
-buttonHeader.addEventListener('click', handler)
-function handler(event) {
-  buttonHeader.style.backgroundColor = 'red';
-}
-
-const button_Portfolio = document.querySelector('.portfolio_buttons')  //a в портфолио
-button_Portfolio.addEventListener('click', handler1);
-function handler1(event) {
-  setTimeout (function () {
-    const target = event.target;
-    target.style.backgroundColor = 'cyan';
-    target.style.color = 'white';
-    }, 2000);
-  event.preventDefault();  
-}
-
-function get_Random_color () {
+function getRandomColor () {
   return '#' + Math.floor(Math.random()*10000000).toString(16);
 }
 
-const button_all = document.querySelector('.page-container') // button везде
-button_all.addEventListener('click', handler2);
-function handler2(event) {
-  const target1 = event.target;
-  const butt = target1.closest('button');
-  if (butt) {
-    target1.style.backgroundColor = get_Random_color();
-    target1.style.color = 'yellow';
-  }
-}
-
-const anchors_buttons = document.querySelector('.page-container');   //a и buttons везде
-anchors_buttons.addEventListener('click', handler3);
-function handler3 (event) {
+const pageContainer = document.querySelector('.page-container');   //a и buttons везде
+pageContainer.addEventListener('click', handler);
+function handler (event) {
   const target = event.target;
   const anchors = target.closest ('a');
   const buttons = target.closest('button');
+  const dat = target.dataset.color;
   if (anchors||buttons) {
-    target.style.backgroundColor = get_Random_color();
-  }
+    target.style.backgroundColor = getRandomColor();
+  };
+  if (dat) {
+    target.style.backgroundColor = 'cyan';
+  };
+  event.preventDefault();
 }

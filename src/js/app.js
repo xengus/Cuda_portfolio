d8e6b37cee58d2a05692dd0ac46ef02a64fbf96c
xengus <serjiox13@gmail.com>
2019-09-05 14:13:33 +0300

    portfolio button

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,18 @@
 function getRandomColor () {
-  return '#' + Math.floor(Math.random()*10000000).toString(16);
+  let sum = '#';
+  const string = 'ABCDEF1234567890';
+  while (sum.length < 7) {
+    sum+=string.charAt(Math.floor(Math.random()*16)+1)
+  }
+  return sum;
 }
 
-const pageContainer = document.querySelector('.page-container');   //a и buttons везде
+const pageContainer = document.querySelector('.portfolio_button');  
 pageContainer.addEventListener('click', handler);
 function handler (event) {
-  const target = event.target;
-  const anchors = target.closest ('a');
-  const buttons = target.closest('button');
-  const dat = target.dataset.color;
-  if (anchors||buttons) {
-    target.style.backgroundColor = getRandomColor();
-  };
-  if (dat) {
-    target.style.backgroundColor = 'cyan';
-  };
   event.preventDefault();
+  const target = event.target;
+  const data = target.dataset.color;
+  let result = (data===target.style.backgroundColor) ? target.style.backgroundColor = getRandomColor(): target.style.backgroundColor = 'pink';
 }
+ 
